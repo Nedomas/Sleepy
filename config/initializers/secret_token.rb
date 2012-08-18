9bf050f7f4a76a70914c77c3b515dbfb2d134b05
Nedomas <domas.bitvinskas@me.com>
2012-08-19 01:41:20 +0300

    Clean commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sleepy::Application.config.secret_token = 'acfd7385c64d5ebdbea69948f01af86e82fcc022ca1b5f7c14a05e8a2086fd43308e89a86ed0fb7edfa387677651af568ec132f4d11656727a5f1a76adba9a3e'
